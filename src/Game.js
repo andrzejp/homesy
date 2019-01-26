7530b9a57aa6c69ee15d8d1cc8c54ed80ac6e208
Joe Wells <joe.wells@aexp.com>
2019-01-26 22:34:35 +0000

    Add logo and text

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,9 @@
 import MainScene from "./scene/MainScene.js";
+import TitleScene from "./scene/TitleScene.js";
 
 const game = new Phaser.Game({
     width: 1024,
     height: 768,
     autoResize: true,
-    scene: [MainScene] //Reference scene classes here
+    scene: [TitleScene, MainScene] //Reference scene classes here
 });
---
+++ src/scene/TitleScene.js
@@ -0,0 +1,20 @@
+class TitleScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'TitleScene'
+        });
+    }
+
+    preload() {
+        this.load.svg('bg', 'assets/bg.svg', {width: this.sys.game.config.width, height: this.sys.game.config.height});
+        this.load.svg('earthman', 'assets/earthman/000.svg', {width: 300, height: 300});
+        this.load.svg('title', 'assets/title/title.svg', {width: 800, height: 285});
+    }
+
+    create() {
+        this.add.image(0, 0, 'bg').setOrigin(0);
+        this.add.image(-50,0, 'earthman').setOrigin(0);
+        this.add.image(200,0, 'title').setOrigin(0);
+    }
+}
+export default TitleScene;
