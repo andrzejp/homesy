b6925fbd46b21647142cdcb6b0ccf8116ee16e14
Andrzej Prochyra <andrzej.prochyra@aexp.com>
2019-01-27 01:08:09 +0000

    Give spaceman some movement on title screen and add some more text

--- src/scene/TitleScene.js
+++ src/scene/TitleScene.js
@@ -15,7 +15,7 @@ class TitleScene extends Phaser.Scene {
 
     create() {
         this.add.image(0, 0, 'bg').setOrigin(0);
-        this.add.image(-50,0, 'earthman').setOrigin(0);
+        this.spaceMan = this.add.image(150, 150, 'earthman');
         this.add.image(200,0, 'title').setOrigin(0);
 
         this.input.manager.enabled = true;
@@ -25,6 +25,39 @@ class TitleScene extends Phaser.Scene {
             this.scene.switch('MainScene');
 
         }, this);
+
+        var add = this.add;
+        var input = this.input;
+
+        WebFont.load({
+            google: {
+                families: [ 'Freckle Face', 'Finger Paint', 'Nosifer' ]
+            },
+            active: function ()
+            {
+                add.text(80, 400, 'The Journey Home...', { fontFamily: 'Freckle Face', fontSize: 80, color: '#ffffff' }).setShadow(2, 2, "#333333", 2, false, true);
+            }
+        });
+
+        this.tweens.timeline({
+
+            targets: this.spaceMan,
+            loop: -1,
+
+            tweens: [
+                {
+                    y: 100,
+                    ease: 'Sine.easeInOut',
+                    duration: 3000,
+                    yoyo: true
+                }
+            ]
+
+        });
+    }
+
+    update() {
+        this.spaceMan.rotation += 0.001;
     }
 }
 export default TitleScene;
