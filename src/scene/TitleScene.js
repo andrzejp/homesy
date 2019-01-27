84f7af6028c6ad5fd083ee14ae59d9ddc6b8f9c4
Joe Wells <joe.wells@aexp.com>
2019-01-27 03:08:02 +0000

    Fix title.svg size and scene adjustments

--- src/scene/TitleScene.js
+++ src/scene/TitleScene.js
@@ -9,8 +9,8 @@ class TitleScene extends Phaser.Scene {
 
     preload() {
         this.load.svg('bg', 'assets/bg.svg', {width: this.sys.game.config.width, height: this.sys.game.config.height});
-        this.load.svg('earthman', 'assets/earthMan.svg', {width: 300, height: 300});
-        this.load.svg('title', 'assets/title.svg', {width: 800, height: 285});
+        this.load.svg('earthman', 'assets/earthMan.svg', {width: 250, height: 250});
+        this.load.svg('title', 'assets/title.svg', {width: 741, height: 139});
 
         this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
     }
@@ -18,7 +18,7 @@ class TitleScene extends Phaser.Scene {
     create() {
         this.add.image(0, 0, 'bg').setOrigin(0);
         this.spaceMan = this.add.image(150, 150, 'earthman');
-        this.add.image(200,0, 'title').setOrigin(0);
+        this.add.image(this.centerX(), this.centerY(), 'title').setOrigin(0.5,1);
 
         this.input.manager.enabled = true;
 
@@ -28,8 +28,8 @@ class TitleScene extends Phaser.Scene {
 
         }, this);
 
-        var add = this.add;
-        var input = this.input;
+        const add = this.add;
+        const input = this.input;
 
         WebFont.load({
             google: {
@@ -37,7 +37,7 @@ class TitleScene extends Phaser.Scene {
             },
             active: function ()
             {
-                add.text(80, 400, 'The Journey Home...', { fontFamily: 'Orbitron', fontSize: 80, color: '#ffffff' }).setShadow(2, 2, "#333333", 2, false, true);
+                add.text(80, 400, 'The Journey Home...', { fontFamily: 'Orbitron', fontSize: 40, color: '#ffffff' }).setShadow(2, 2, "#333333", 2, false, true);
             }
         });
 
@@ -60,6 +60,15 @@ class TitleScene extends Phaser.Scene {
 
     update() {
         this.spaceMan.rotation += 0.001;
+    }
+
+    centerX ()
+    {
+        return this.sys.game.config.width / 2;
+    }
+    centerY ()
+    {
+        return this.sys.game.config.height / 2;
     }
 }
 export default TitleScene;
